782c773f7ebee3f821afdd2ec1c0cf4007507279
Djamal dos Santos <leondark.tzk@gmail.com>
2020-08-06 08:07:02 +0200

    Actualizacao do Layout principal (Mudanca para AdminLte3)

---
+++ web/layouts/_default.vue
@@ -0,0 +1,229 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" elevation="0">
+      <v-app-bar-nav-icon @click.stop="sidebarMenu = !sidebarMenu" />
+      <v-spacer></v-spacer>
+      <v-icon>mdi-brightness-3</v-icon>
+      <v-btn color="primary" class="mr-2" @click="toggleTheme">
+        <v-icon>{{ iconType }}</v-icon>
+      </v-btn>
+      <v-icon>mdi-brightness-7</v-icon>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="sidebarMenu"
+      app
+      floating
+      :permanent="sidebarMenu"
+      :mini-variant.sync="mini"
+    >
+      <v-list nav color="primary" dark>
+        <v-list-item dense>
+          <v-list-item-action>
+            <v-icon @click.stop="sidebarMenu = !sidebarMenu">
+              mdi-chevron-left
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              <h3 class="font-weight-thin">ILULU Dashboard</h3>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="toggleMini = !toggleMini">
+          <v-list-item-avatar>
+            <v-icon left>mdi-account-outline</v-icon>
+          </v-list-item-avatar>
+          <v-list-item-content class="text-truncate">
+            Nome Sobrenome
+          </v-list-item-content>
+          <v-btn icon small>
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list>
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+            :to="item.href"
+          >
+            <v-list-item-icon>
+              <v-icon>
+                {{ item.icon }}
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.title }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click="logoutUser">
+            <v-list-item-icon>
+              <v-icon>
+                mdi-power
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>
+                Logout
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container class="px-4 py-0 fill-height" fluid>
+        <v-row class="fill-height">
+          <v-col>
+            <transition name="fade">
+              <nuxt />
+            </transition>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <v-footer class="py-3">
+      <span class="ml-auto overline">
+        ILULU &copy;2020. Developed by FireDevz
+      </span>
+    </v-footer>
+  </v-app>
+</template>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-property: opacity;
+  transition-duration: 0.25s;
+}
+
+.fade-enter-active {
+  transition-delay: 0.25s;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+/*
+.v-navigation-drawer__content {
+  background-color: red;
+  background-color: #03a9f4 !important;
+  border-color: #03a9f4 !important;
+}
+*/
+
+.theme--light.v-navigation-drawer {
+  background: #eee;
+}
+
+.theme--light.v-application::-webkit-scrollbar {
+  width: 13px;
+}
+
+.theme--light.v-application::-webkit-scrollbar-track {
+  background: #e6e6e6;
+  border-left: 1px solid #dadada;
+}
+
+.theme--light.v-application::-webkit-scrollbar-thumb {
+  background: #b0b0b0;
+  border: solid 3px #e6e6e6;
+  border-radius: 7px;
+}
+
+.theme--light.v-application::-webkit-scrollbar-thumb:hover {
+  background: red;
+}
+
+.theme--dark.v-application::-webkit-scrollbar {
+  width: 13px;
+}
+
+.theme--dark.v-application::-webkit-scrollbar-track {
+  background: #202020;
+  border-left: 1px solid #2c2c2c;
+}
+
+.theme--dark.v-application::-webkit-scrollbar-thumb {
+  background: #3e3e3e;
+  border: solid 3px #202020;
+  border-radius: 7px;
+}
+
+.theme--dark.v-application::-webkit-scrollbar-thumb:hover {
+  background: red;
+}
+</style>
+<script>
+import { mdiMenu } from '@mdi/js'
+
+export default {
+  middleware: 'authenticated',
+  data() {
+    return {
+      mdi_menu: mdiMenu,
+      sidebarMenu: true,
+      toggleMini: false,
+      isDark: true,
+      items: [
+        {
+          title: 'Inicio',
+          href: '/',
+          icon: 'mdi-home-outline'
+        },
+        {
+          title: 'Contas',
+          href: '/contas',
+          icon: 'mdi-shield-account'
+        },
+        {
+          title: 'Equipamentos',
+          href: '/equipamentos',
+          icon: 'mdi-stove'
+        },
+        {
+          title: 'Operadores',
+          href: '/operadores',
+          icon: 'mdi-shield-account'
+        },
+        {
+          title: 'Residentes',
+          href: '/residentes',
+          icon: 'mdi-shield-account'
+        },
+        {
+          title: 'Visitantes',
+          href: '/visitantes',
+          icon: 'mdi-account-clock-outline'
+        },
+        {
+          title: 'Settings',
+          href: '/settings',
+          icon: 'mdi-cog-outline'
+        }
+      ]
+    }
+  },
+  computed: {
+    mini() {
+      return this.$vuetify.breakpoint.smAndDown || this.toggleMini
+    },
+    iconType() {
+      return !this.$vuetify.theme.dark ? 'mdi-ray-end' : 'mdi-ray-start'
+    }
+  },
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      this.isDark = !this.isDark
+    },
+    logoutUser() {
+      this.$store.dispatch('logout')
+      this.$nuxt.$router.replace({ path: '/login' })
+    }
+  }
+}
+</script>
--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,229 +1,108 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" elevation="0">
-      <v-app-bar-nav-icon @click.stop="sidebarMenu = !sidebarMenu" />
-      <v-spacer></v-spacer>
-      <v-icon>mdi-brightness-3</v-icon>
-      <v-btn color="primary" class="mr-2" @click="toggleTheme">
-        <v-icon>{{ iconType }}</v-icon>
-      </v-btn>
-      <v-icon>mdi-brightness-7</v-icon>
-    </v-app-bar>
-    <v-navigation-drawer
-      v-model="sidebarMenu"
-      app
-      floating
-      :permanent="sidebarMenu"
-      :mini-variant.sync="mini"
-    >
-      <v-list nav color="primary" dark>
-        <v-list-item dense>
-          <v-list-item-action>
-            <v-icon @click.stop="sidebarMenu = !sidebarMenu">
-              mdi-chevron-left
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>
-              <h3 class="font-weight-thin">ILULU Dashboard</h3>
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item @click="toggleMini = !toggleMini">
-          <v-list-item-avatar>
-            <v-icon left>mdi-account-outline</v-icon>
-          </v-list-item-avatar>
-          <v-list-item-content class="text-truncate">
-            Nome Sobrenome
-          </v-list-item-content>
-          <v-btn icon small>
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
-        </v-list-item>
-        <v-divider></v-divider>
-        <v-list>
-          <v-list-item
-            v-for="item in items"
-            :key="item.title"
-            link
-            :to="item.href"
+  <div>
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+            <i class="fas fa-bars"></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="/" class="brand-link" style="text-align:center; margin:auto;">
+        <span>ILULU</span>
+      </a>
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <nav class="mt-2">
+          <ul
+            class="nav nav-pills nav-sidebar flex-column"
+            data-widget="treeview"
+            role="menu"
+            data-accordion="false"
           >
-            <v-list-item-icon>
-              <v-icon>
-                {{ item.icon }}
-              </v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>
-                {{ item.title }}
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item @click="logoutUser">
-            <v-list-item-icon>
-              <v-icon>
-                mdi-power
-              </v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>
-                Logout
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-list>
-    </v-navigation-drawer>
-    <v-main>
-      <v-container class="px-4 py-0 fill-height" fluid>
-        <v-row class="fill-height">
-          <v-col>
-            <transition name="fade">
-              <nuxt />
-            </transition>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-    <v-footer class="py-3">
-      <span class="ml-auto overline">
-        ILULU &copy;2020. Developed by FireDevz
-      </span>
-    </v-footer>
-  </v-app>
+            <li v-for="item in items" :key="item.title" class="nav-item">
+              <a :href="item.href" class="nav-link">
+                <i :class="item.icon"></i>
+                <p>{{ item.title }}</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" @click="save">
+                <i class="nav-icon fas fa-power-off"></i>
+                <p>Logout</p>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </aside>
+    <div class="content-wrapper">
+      <transition name="fade">
+        <nuxt />
+      </transition>
+    </div>
+    <!-- Main Footer -->
+    <footer class="main-footer">
+      <!-- To the right -->
+      <div class="float-right d-none d-sm-inline">
+        <strong>Developed by FIREDEV</strong>
+      </div>
+      <!-- Default to the left -->
+      <strong>Copyright &copy; 2020 ILULU.</strong> All rights reserved.
+    </footer>
+  </div>
 </template>
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition-property: opacity;
-  transition-duration: 0.25s;
-}
-
-.fade-enter-active {
-  transition-delay: 0.25s;
-}
-
-.fade-enter,
-.fade-leave-active {
-  opacity: 0;
-}
-/*
-.v-navigation-drawer__content {
-  background-color: red;
-  background-color: #03a9f4 !important;
-  border-color: #03a9f4 !important;
-}
-*/
-
-.theme--light.v-navigation-drawer {
-  background: #eee;
-}
-
-.theme--light.v-application::-webkit-scrollbar {
-  width: 13px;
-}
-
-.theme--light.v-application::-webkit-scrollbar-track {
-  background: #e6e6e6;
-  border-left: 1px solid #dadada;
-}
-
-.theme--light.v-application::-webkit-scrollbar-thumb {
-  background: #b0b0b0;
-  border: solid 3px #e6e6e6;
-  border-radius: 7px;
-}
-
-.theme--light.v-application::-webkit-scrollbar-thumb:hover {
-  background: red;
-}
-
-.theme--dark.v-application::-webkit-scrollbar {
-  width: 13px;
-}
-
-.theme--dark.v-application::-webkit-scrollbar-track {
-  background: #202020;
-  border-left: 1px solid #2c2c2c;
-}
-
-.theme--dark.v-application::-webkit-scrollbar-thumb {
-  background: #3e3e3e;
-  border: solid 3px #202020;
-  border-radius: 7px;
-}
-
-.theme--dark.v-application::-webkit-scrollbar-thumb:hover {
-  background: red;
-}
-</style>
 <script>
-import { mdiMenu } from '@mdi/js'
-
 export default {
-  middleware: 'authenticated',
   data() {
     return {
-      mdi_menu: mdiMenu,
-      sidebarMenu: true,
-      toggleMini: false,
-      isDark: true,
+      nav: 'main-header navbar navbar-expand navbar-white navbar-light',
       items: [
         {
           title: 'Inicio',
           href: '/',
-          icon: 'mdi-home-outline'
+          icon: 'nav-icon fas fa-home'
         },
         {
           title: 'Contas',
           href: '/contas',
-          icon: 'mdi-shield-account'
+          icon: 'nav-icon fas fa-shield-alt'
         },
         {
           title: 'Equipamentos',
           href: '/equipamentos',
-          icon: 'mdi-stove'
+          icon: 'nav-icon fas fa-box'
         },
         {
           title: 'Operadores',
           href: '/operadores',
-          icon: 'mdi-shield-account'
+          icon: 'nav-icon fas fa-user-shield'
         },
         {
           title: 'Residentes',
           href: '/residentes',
-          icon: 'mdi-shield-account'
+          icon: 'nav-icon fas fa-users'
         },
         {
           title: 'Visitantes',
           href: '/visitantes',
-          icon: 'mdi-account-clock-outline'
-        },
-        {
-          title: 'Settings',
-          href: '/settings',
-          icon: 'mdi-cog-outline'
+          icon: 'nav-icon fas fa-user'
         }
       ]
     }
   },
-  computed: {
-    mini() {
-      return this.$vuetify.breakpoint.smAndDown || this.toggleMini
-    },
-    iconType() {
-      return !this.$vuetify.theme.dark ? 'mdi-ray-end' : 'mdi-ray-start'
+  methods: {
+    save() {
+      console.log('asas')
     }
   },
-  methods: {
-    toggleTheme() {
-      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
-      this.isDark = !this.isDark
-    },
-    logoutUser() {
-      this.$store.dispatch('logout')
-      this.$nuxt.$router.replace({ path: '/login' })
-    }
-  }
+  head: {}
 }
 </script>
